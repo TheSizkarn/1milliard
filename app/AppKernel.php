43a9188d3e2074af8f0fd8b8a07fb5ae945c4d08
TheSizkarn <thesizkarn@gmail.com>
2018-02-10 03:05:45 +0100

    install UserBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new RC\UserBundle\RCUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/RC/UserBundle/RCUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RC\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RCUserBundle extends Bundle
+{
+}
